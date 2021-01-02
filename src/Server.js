ed29476b1ba1022106485739d51328d47888ab37
Jon Lippincott <jonlippincott@gmail.com>
2021-01-02 22:25:43 +0100

    Fix exception text (date -> timestamp)

--- src/Server.js
+++ src/Server.js
@@ -124,7 +124,7 @@ class SimpleHMACAuth {
 
     if (request.headers.timestamp === undefined) {
 
-      throw new AuthError(`Missing timestamp. Please timestamp all incoming requests by including 'date' header.`, `DATE_HEADER_MISSING`);
+      throw new AuthError(`Missing timestamp. Please timestamp all incoming requests by including 'timestamp' header.`, `DATE_HEADER_MISSING`);
     }
 
     // First, confirm that the 'date' header is recent enough
